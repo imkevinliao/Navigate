cd7cd393aa6492ec2d8582d0bc174401c08f6aaf
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2023-03-17 16:12:26 +0800

    Update main.py

--- projects/stock/main.py
+++ projects/stock/main.py
@@ -1,1 +1,5 @@
+from ak import AnalyzeStock
 
+inst = AnalyzeStock(csvfile=r"D:\lwx1135395work\PycharmProjects\HISI\res\513330.csv")
+inst.basic_info()
+inst.query_compare(compare_price=0.407, plot=True, start_time="20220316")
